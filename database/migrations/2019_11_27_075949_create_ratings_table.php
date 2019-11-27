197e05d0b7143aabc26c8aa8ba6173aaa427b5ae
kyawmoehan <kyawmoehan012@gmail.com>
2019-11-27 14:57:13 +0630

    rating table delete and create

--- database/migrations/2019_11_27_075949_create_ratings_table.php
+++ database/migrations/2019_11_27_075949_create_ratings_table.php
@@ -2,32 +2,31 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class CreateRatingsTable extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
         Schema::create('ratings', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('menu_id');
-            $table->float('rating');
+            $table->increments('id');
             $table->timestamps();
+            $table->integer('rating');
+            $table->morphs('rateable');
+            $table->bigInteger('user_id')->unsigned();
+            $table->index('rateable_id');
+            $table->index('rateable_type');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
-        Schema::dropIfExists('ratings');
+        Schema::drop('ratings');
     }
 }
